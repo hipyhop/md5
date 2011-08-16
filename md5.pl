476f5dddac4eee5bfc679166b9fd04a7d9b57e77
tomfluff <tomfluff@gmail.com>
2011-08-16 23:01:04 +0100

    Moved subroutines below main program code, few tweaks to confirm with perlcritic -3

--- md5.pl
+++ md5.pl
@@ -2,77 +2,23 @@
 use strict;
 use warnings;
 
-use v5.10;
+use 5.010;
 use Getopt::Long;
 use Digest::MD5;
 
 #Determine if digesting files or strings
 my $file   = 0;
 my $output = '';
+
 GetOptions(
            "file|f"   => \$file,
            "output|o" => \$output
-          ) or die("Error reading arguments");
+          ) or die "Error reading arguments\n";
 
-#Digest multiple strings
-sub md5_string
-{
-  my $hasher = Digest::MD5->new;
-
-  foreach (@_)
-  {
-    $hasher->add($_);
-
-    #print hex digest and clear for next $val
-    say "$_ => ", $hasher->hexdigest;
-  }
-}
-
-#Digest multiple files
-sub md5_file
-{
-  my $hasher = Digest::MD5->new;
-
-  if ($output)
-  {
-    if (open(my $fh, '>', 'checksums.md5'))
-    {
-      select $fh;
-      printf(
-        "#Checksum file created on %02d/%02d/%02d\n",
-        sub { ($_[3], $_[4] + 1, $_[5] + 1900) }
-          ->(localtime)
-      );
-    }
-    else
-    {
-      warn "Could not open 'checksums.md5' for output\n";
-    }
-  }
-
-  foreach (@_)
-  {
-    if (!open(my $file, "<", $_))
-    {
-      say "$_ => Error opening file";
-    }
-    else
-    {
-      $hasher->addfile($file);
-
-      #print hex digest and clear for next $val
-      say "$_ => ", $hasher->hexdigest;
-    }
-  }
-  select *STDOUT;
-}
-
+# Main program
 if (@ARGV > 0)
 {
-  if ($output && !$file)
-  {
-    die "--output must be used with --file\n";
-  }
+  die "--output must be used with --file\n" if($output && !$file);
 
   if ($file)
   {
@@ -86,4 +32,63 @@ if (@ARGV > 0)
 else
 {
   die "Usage: $0 string OR $0 -f filename\n";
+}
+
+#Digest multiple strings
+sub md5_string
+{
+  my @strings = @_;
+  my $hasher = Digest::MD5->new;
+
+  foreach (@strings)
+  {
+    $hasher->add($_);
+
+    #print hex digest and clear for next $val
+    say "$_ => ", $hasher->hexdigest;
+  }
+  return 1;
+}
+
+#Digest multiple files
+sub md5_file
+{
+  my @filenames = @_;
+  my $hasher = Digest::MD5->new;
+
+  if ($output)
+  {
+    if (open(my $OUTH, '>', 'checksums.md5'))
+    {
+      select $OUTH;
+      printf(
+        "#Checksum file created on %02d/%02d/%02d\n",
+        sub { ($_[3], $_[4] + 1, $_[5] + 1900) }
+          ->(localtime)
+      );
+    }
+    else
+    {
+      warn "Could not open 'checksums.md5' for output\n";
+    }
+  }
+
+  foreach (@filenames)
+  {
+    if (!open(my $FH, "<", $_))
+    {
+      say "$_ => Error opening file";
+    }
+    else
+    {
+      $hasher->addfile($FH);
+      close $FH;
+
+      #print hex digest and clear for next $val
+      say "$_ => ", $hasher->hexdigest;
+    }
+  }
+  close $OUTH
+  select *STDOUT;
+  return 1;
 }
