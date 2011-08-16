2beadac9ee3c30e7fe047f6b7cab739a0150e54d
tomfluff <tomfluff@gmail.com>
2011-08-16 23:04:20 +0100

    Ran files through perltidy with options -gnu

--- md5.pl
+++ md5.pl
@@ -18,77 +18,76 @@ GetOptions(
 # Main program
 if (@ARGV > 0)
 {
-  die "--output must be used with --file\n" if($output && !$file);
+    die "--output must be used with --file\n" if ($output && !$file);
 
-  if ($file)
-  {
-    md5_file(@ARGV);
-  }
-  else
-  {
-    md5_string(@ARGV);
-  }
+    if ($file)
+    {
+        md5_file(@ARGV);
+    }
+    else
+    {
+        md5_string(@ARGV);
+    }
 }
 else
 {
-  die "Usage: $0 string OR $0 -f filename\n";
+    die "Usage: $0 string OR $0 -f filename\n";
 }
 
 #Digest multiple strings
 sub md5_string
 {
-  my @strings = @_;
-  my $hasher = Digest::MD5->new;
+    my @strings = @_;
+    my $hasher  = Digest::MD5->new;
 
-  foreach (@strings)
-  {
-    $hasher->add($_);
+    foreach (@strings)
+    {
+        $hasher->add($_);
 
-    #print hex digest and clear for next $val
-    say "$_ => ", $hasher->hexdigest;
-  }
-  return 1;
+        #print hex digest and clear for next $val
+        say "$_ => ", $hasher->hexdigest;
+    }
+    return 1;
 }
 
 #Digest multiple files
 sub md5_file
 {
-  my @filenames = @_;
-  my $hasher = Digest::MD5->new;
+    my @filenames = @_;
+    my $hasher    = Digest::MD5->new;
 
-  if ($output)
-  {
-    if (open(my $OUTH, '>', 'checksums.md5'))
+    if ($output)
     {
-      select $OUTH;
-      printf(
-        "#Checksum file created on %02d/%02d/%02d\n",
-        sub { ($_[3], $_[4] + 1, $_[5] + 1900) }
-          ->(localtime)
-      );
+        if (open(my $OUTH, '>', 'checksums.md5'))
+        {
+            select $OUTH;
+            printf(
+                "#Checksum file created on %02d/%02d/%02d\n",
+                sub { ($_[3], $_[4] + 1, $_[5] + 1900) }
+                  ->(localtime)
+            );
+        }
+        else
+        {
+            warn "Could not open 'checksums.md5' for output\n";
+        }
     }
-    else
-    {
-      warn "Could not open 'checksums.md5' for output\n";
-    }
-  }
 
-  foreach (@filenames)
-  {
-    if (!open(my $FH, "<", $_))
+    foreach (@filenames)
     {
-      say "$_ => Error opening file";
-    }
-    else
-    {
-      $hasher->addfile($FH);
-      close $FH;
+        if (!open(my $FH, "<", $_))
+        {
+            say "$_ => Error opening file";
+        }
+        else
+        {
+            $hasher->addfile($FH);
+            close $FH;
 
-      #print hex digest and clear for next $val
-      say "$_ => ", $hasher->hexdigest;
+            #print hex digest and clear for next $val
+            say "$_ => ", $hasher->hexdigest;
+        }
     }
-  }
-  close $OUTH
-  select *STDOUT;
-  return 1;
+    close $OUTH select *STDOUT;
+    return 1;
 }
--- test.pl
+++ test.pl
@@ -14,171 +14,172 @@ test3();
 # Test1 - Checks md5 checksum of string 'test' matches known checksum
 sub test1
 {
-  my $testname        = 'Known string md5 as expected';
-  my $expected_result = '098f6bcd4621d373cade4e832627b4f6';
+    my $testname        = 'Known string md5 as expected';
+    my $expected_result = '098f6bcd4621d373cade4e832627b4f6';
 
-  #execute md5.pl with single argument and store output
-  my @result = `perl md5.pl test`;
+    #execute md5.pl with single argument and store output
+    my @result = `perl md5.pl test`;
 
-  #Check command executed successfully
-  diag("$!\n") if ($? == -1);
+    #Check command executed successfully
+    diag("$!\n") if ($? == -1);
 
-  #Scrape only resulting hash
-  my $checksum = get_md5_from_string($result[0]);
+    #Scrape only resulting hash
+    my $checksum = get_md5_from_string($result[0]);
 
-  #Check checksum is valid
-  diag("Invalid checksum") if (!is_checksum($checksum));
+    #Check checksum is valid
+    diag("Invalid checksum") if (!is_checksum($checksum));
 
-  ok($checksum eq $expected_result, $testname);
+    ok($checksum eq $expected_result, $testname);
 }
 
 # Test2, writes a-zA-Z0-9 to a file and md5s the contents
 sub test2
 {
-  my $testname        = 'Known file md5 as expected';
-  my $expected_result = 'c5721e84f80f551cec9f252473d47ced';
+    my $testname        = 'Known file md5 as expected';
+    my $expected_result = 'c5721e84f80f551cec9f252473d47ced';
 
-  my $filename = 'all_chars.txt';
-  open(my $fh, '>', $filename);
+    my $filename = 'all_chars.txt';
+    open(my $fh, '>', $filename);
 
-  #Opened file, write test chars to it
-  print $fh 'a' .. 'z', "\n";
-  print $fh 'A' .. 'Z', "\n";
-  print $fh '0' .. '9', "\n";
-  close($fh);
+    #Opened file, write test chars to it
+    print $fh 'a' .. 'z', "\n";
+    print $fh 'A' .. 'Z', "\n";
+    print $fh '0' .. '9', "\n";
+    close($fh);
 
-  #md5 the file and grab the results;
-  my @result = `perl md5.pl -f all_chars.txt`;
+    #md5 the file and grab the results;
+    my @result = `perl md5.pl -f all_chars.txt`;
 
-  #Check command executed successfully
-  diag("$!") if ($? == -1);
+    #Check command executed successfully
+    diag("$!") if ($? == -1);
 
-  #Scrape only resulting hash
-  my $checksum = get_md5_from_string($result[0]);
+    #Scrape only resulting hash
+    my $checksum = get_md5_from_string($result[0]);
 
-  #Check checksum is valid
-  diag("Invalid Checksum") if (!is_checksum($checksum));
+    #Check checksum is valid
+    diag("Invalid Checksum") if (!is_checksum($checksum));
 
-  ok($checksum eq $expected_result, $testname);
-  remove_files($filename);
+    ok($checksum eq $expected_result, $testname);
+    remove_files($filename);
 }
 
 # Test 3 - Checks the --output option with no args
 sub test3
 {
-  my $testname = "--output created 'checksums.md5'";
-  my $CHECKSUM_FILE = 'checksums.md5';    #Name of auto generated checksum file
-  my $CHECKSUM_FILE_SEPERATOR =
-    ' => ';    #Char sequence separating filenames and checksums
+    my $testname = "--output created 'checksums.md5'";
+    my $CHECKSUM_FILE = 'checksums.md5';   #Name of auto generated checksum file
+    my $CHECKSUM_FILE_SEPERATOR =
+      ' => ';    #Char sequence separating filenames and checksums
 
-  #Names of junk files to be testes
-  my $junkfile1 = './tmp_junkfile1.jnk';
-  my $junkfile2 = './tmp_junkfile2.jnk';
+    #Names of junk files to be testes
+    my $junkfile1 = './tmp_junkfile1.jnk';
+    my $junkfile2 = './tmp_junkfile2.jnk';
 
-  #Cleanup any pre-existing junk files
-  remove_files($CHECKSUM_FILE, $junkfile1, $junkfile2);
+    #Cleanup any pre-existing junk files
+    remove_files($CHECKSUM_FILE, $junkfile1, $junkfile2);
 
-  #create the junk files before testing
-  make_junk_file($junkfile1, 256);
-  make_junk_file($junkfile2, 1024);
+    #create the junk files before testing
+    make_junk_file($junkfile1, 256);
+    make_junk_file($junkfile2, 1024);
 
-  #Check the --output option with no args actually creates a 'checksums.md5' file
-  `perl md5.pl --file --output $junkfile1 $junkfile2`;
+    #Check the --output option with no args actually creates a 'checksums.md5' file
+    `perl md5.pl --file --output $junkfile1 $junkfile2`;
 
-  #Check command executed successfully
-  ok($? != -1, "Output to file successful");
+    #Check command executed successfully
+    ok($? != -1, "Output to file successful");
 
-  #Check File exists
-  ok(open(my $fh, '<', $CHECKSUM_FILE), "File Exists");
+    #Check File exists
+    ok(open(my $fh, '<', $CHECKSUM_FILE), "File Exists");
 
-  #Split 'checksums.md5' into hash of filenames => md5sum
-  my %results;
+    #Split 'checksums.md5' into hash of filenames => md5sum
+    my %results;
 
-  while (<$fh>)
-  {
-    if (!/^#/)    #Only check if line does NOT start with #
+    while (<$fh>)
     {
-      my ($filename, $checksum) = split(/$CHECKSUM_FILE_SEPERATOR/);
-      $checksum =~ s/\n//g;    #Remove any new lines
-      $results{$filename} = $checksum;
+        if (!/^#/)    #Only check if line does NOT start with #
+        {
+            my ($filename, $checksum) = split(/$CHECKSUM_FILE_SEPERATOR/);
+            $checksum =~ s/\n//g;    #Remove any new lines
+            $results{$filename} = $checksum;
+        }
     }
-  }
-  close($fh);
+    close($fh);
 
-  while (my ($fname, $chksum) = each %results)
-  {
-    ok(md5_file($fname) eq $chksum, "Junk file checksum verified");
-  }
+    while (my ($fname, $chksum) = each %results)
+    {
+        ok(md5_file($fname) eq $chksum, "Junk file checksum verified");
+    }
 
-  #cleanup files after testing
-  remove_files($CHECKSUM_FILE, $junkfile1, $junkfile2);
+    #cleanup files after testing
+    remove_files($CHECKSUM_FILE, $junkfile1, $junkfile2);
 }
 
 #create a random textfile
 sub make_junk_file
 {
-  my ($filename, $length) = @_;
+    my ($filename, $length) = @_;
 
-  if (open(my $fh, '>', $filename))
-  {
-    my @chars = ('A' .. 'Z', 'a' .. 'z', 0 .. 9, "\n");
-    my $contents = '';
-    while ($length > 0)
+    if (open(my $fh, '>', $filename))
     {
-      $contents .= $chars[rand(@chars)];
-      $length--;
+        my @chars = ('A' .. 'Z', 'a' .. 'z', 0 .. 9, "\n");
+        my $contents = '';
+        while ($length > 0)
+        {
+            $contents .= $chars[rand(@chars)];
+            $length--;
+        }
+        print($fh $contents);
+        close($fh);
+        return 1;
     }
-    print($fh $contents);
-    close($fh);
-    return 1;
-  }
-  else
-  {
-    print "Error creating file $filename: $!\n";
-    return 0;
-  }
+    else
+    {
+        print "Error creating file $filename: $!\n";
+        return 0;
+    }
 }    #end make_junk_file
 
 #Verify checksum is a valid checksum
 sub is_checksum
 {
-  return $_[0] =~ /[\p{Hex}]{32}/;
+    return $_[0] =~ /[\p{Hex}]{32}/;
 }
 
 #Extract MD5 checksum from string, return empty string on fail
 sub get_md5_from_string
 {
-  if ($_[0] =~ /([a-fA-F0-9]{32})/)
-  {
-    return $1;
-  }
-  return '';
+    if ($_[0] =~ /([a-fA-F0-9]{32})/)
+    {
+        return $1;
+    }
+    return '';
 }
 
 #Attempt to delete all filenames that are passed
 sub remove_files
 {
-  foreach (@_)
-  {
-    if (open(my $fh, '<', $_))
+    foreach (@_)
     {
-      unlink $_ or warn "Could not unlink $_ : $!";
+        if (open(my $fh, '<', $_))
+        {
+            unlink $_ or warn "Could not unlink $_ : $!";
+        }
     }
-  }
 }
 
 #Digest a file returning the md5 checksum
 sub md5_file
 {
-  if (open(my $fh, '<', $_[0]))
-  {
-    return Digest::MD5->new->addfile($fh)->hexdigest;
-  }
-  else
-  {
-    #could not read file
-    return 0;
-  }
+    if (open(my $fh, '<', $_[0]))
+    {
+        return Digest::MD5->new->addfile($fh)->hexdigest;
+    }
+    else
+    {
+
+        #could not read file
+        return 0;
+    }
 }
 
 done_testing();
